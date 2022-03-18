67ebf246377195881fdc45ad53e3e06c037b037a
Joe Zanini <jzanini@ucsc.edu>
2022-03-18 12:34:20 -0700

    getting started using David's code as a guide

---
+++ meetings/delete/delete_meeting.js
@@ -0,0 +1,26 @@
+/**
+ *                _               
+ *  __      _____| |__   _____  __
+ *  \ \ /\ / / _ \ '_ \ / _ \ \/ /
+ *   \ V  V /  __/ |_) |  __/>  <         @WebexDevs
+ *    \_/\_/ \___|_.__/ \___/_/\_\
+ * 
+ * DELETE a meeting in Webex with the REST API in Node
+ * https://developer.webex.com/docs/api/v1/meetings/delet-a-meeting
+ * 
+ * Step 0: Have a (free) Webex account: https://cart.webex.com/sign-up
+ * Step 1: Log in to https://developer.webex.com/login
+ * Step 2: Find your bearer token at
+ *         https://developer.webex.com/docs/getting-started under "Your
+ *         Personal Access Token" in the middle of the page.
+ * Step 3: Replace the string on the line that defines const myWebexDeveloperToken,
+ *         just below, with your personal bearer (access) token. Hit "save".
+ * Step 4: Run this file with node from within
+ *         this directory on the command line:
+ * 
+ *         node ./delete_meeting.js
+ * 
+ * Step 5: Profit. Get your app listed in the Webex App Hub!
+ *         https://apphub.webex.com/
+ * 
+ */
