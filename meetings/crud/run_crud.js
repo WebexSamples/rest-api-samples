49674a3a3e0d63fadb34cae79e1cbb16686a9342
David Liedle <dliedle@cisco.com>
2022-08-17 14:35:20 -0600

    Renamed and added instructions

--- meetings/crud/run_crud.js
+++ meetings/crud/run_crud.js
@@ -1,11 +1,27 @@
 /**
- *  ____   ___   ____
- * |  _ \ / _ \ / ___|
- * | |_) | | | | |
- * |  __/| |_| | |___
- * |_|    \___/ \____|
+ *                _
+ *  __      _____| |__   _____  __
+ *  \ \ /\ / / _ \ '_ \ / _ \ \/ /
+ *   \ V  V /  __/ |_) |  __/>  <         @WebexDevs
+ *    \_/\_/ \___|_.__/ \___/_/\_\
  *
- * This is a Proof-Of-Concept (POC) for async with our REST API Meetings CRUD project.
+ * CREATE, READ, UPDATE, and DELETE a meeting in Webex with the REST API in Node
+ * https://developer.webex.com/docs/api/v1/meetings/create-a-meeting
+ *
+ * Step 0: Have a (free) Webex account: https://cart.webex.com/sign-up
+ * Step 1: Log in to https://developer.webex.com/login
+ * Step 2: Find your bearer token at
+ *         https://developer.webex.com/docs/getting-started under "Your
+ *         Personal Access Token" in the middle of the page.
+ * Step 3: Replace the string on the line that defines const myWebexDeveloperToken,
+ *         just below, with your personal bearer (access) token. Hit "save".
+ * Step 4: Run this file with node from within
+ *         this directory on the command line:
+ *
+ *         node ./run_crud.js
+ *
+ * Step 5: Profit. Get your app listed in the Webex App Hub!
+ *         https://apphub.webex.com/
  *
  */
 
